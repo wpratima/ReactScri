ed52b94e181d0d538061bd255d324507d7d3dd35
Pratima <pratimawaghmare44@gmail.com>
2021-08-02 17:37:05 +0530

    Worked on handling onclick events and lifecycle methods.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,43 @@ import TimeOfDay from './TimeOfDay';
 import TodoItem from './TodoItem';
 import todosData from './todosData';
 import React from 'react';
+import Counter from './Counter';
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
-      todos : todosData
+        todos: todosData
     }
-  }
-  render(){
-    const todosComponents = this.state.todos.map( todo=>{
-      return <TodoItem todo = {todo}/>
+    this.handleChange = this.handleChange.bind(this)
+}
+  
+  handleChange(id){
+    console.log("in handlechange", id)
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        console.log("in handlechange", todo.completed)
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
     })
+  }
+
+  render(){
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
     return (
       <div className='todo-list'>
         <Header/>
-        {todosComponents}
+        <Counter/>
+        {todoItems}
       </div>
     );
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react"
 
-function TodoItem (props){
-    console.log(props)
-    return(
-        <div className='todo-item' key={props.todo.id}>
-            <input type='checkbox' checked = {props.todo.completed} onChange = {()=>{console.log('checked')}}/>
-            <p>{props.todo.text}</p>
+function TodoItem(props) {
+    return (
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p>{props.item.text}</p>
         </div>
-    
-    );
+    )
 }
-export default TodoItem;
+
+export default TodoItem
