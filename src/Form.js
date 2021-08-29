6f0067006a1c20014e3b1478ee71d8e348397a29
Pratima <pratimawaghmare44@gmail.com>
2021-08-29 14:11:41 +0530

    Added form screen

---
+++ src/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Form extends Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <h1>hi there</h1>
+        );
+    }
+}
